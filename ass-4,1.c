5e922b39e601bbdf8cfb7a31d5bbc410ec9e27b7
rajpoot-shivamsingh <111384353+rajpoot-shivamsingh@users.noreply.github.com>
2022-09-17 00:00:20 +0530

    Add files via upload

---
+++ ass-4,1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a=10;
+	char b='m',d='y',e='s',f='i',g='r',h='G';
+	float c=1.5;
+	// 1. Write a C program to print Hello Students on the screen.
+	printf("hello student\n");	// Ass-4  Q.1 
+	getch();
+	printf(" hello\n student\n");	// 2. Write a C Program to print Hello on the first line and Students in the second line.
+	getch();
+	printf("\\n %%d\n");	//Q. no. 5-6	 Write a C program to print \n  and %d on the screen
+	getch();
+	printf(" %d\n %c%c%c%c%c%c\n %f",a,b,d,e,f,g,h,c);	// Q. no. 7 - Write a C program containing declaration of three variables
+	//getch();
+}
+/*	Q.no. 10	How to convert a Decimal number into a Binary number and vice versa
+0.7 * 2 = 1.4 =1
+0.4 * 2 = 0.8 =0
+0.8 * 2 = 1.6 =1
+0.6 * 2 = 1.2 =1
+0.2 * 2 = 0.4 =0
+0.4 * 2 = 0.8 =0
+binary of 0.7 = 101100	*/
